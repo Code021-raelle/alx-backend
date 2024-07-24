496ee563ac17ab670af32c46a9fb1c8468b0707b
Code021-raelle <gabrielakinshola3@gmail.com>
2024-07-24 17:18:49 +0100

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,32 +1,33 @@
 #!/usr/bin/env python3
+"""First-In First-Out caching module.
 """
-FIFOCache module
-"""
+from collections import OrderedDict
 
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """ FIFOCache is a caching system with FIFO eviction policy """
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """ Initialize FIFOCache """
+        """Initializes the cache.
+        """
         super().__init__()
-        self.order = []
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is None and item is not None:
-            if key in self.cache_data:
-                self.order.remove(key)
-            self.cache_data[key] = item
-            self.order.append(key)
-
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                first_in_key = self.order.pop(0)
-                del self.cache_data[first_in_key]
-                print("DISCARD: {}".format(first_in_key))
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
 
     def get(self, key):
-        """ Get an item by key """
+        """Retrieves an item by key.
+        """
         return self.cache_data.get(key, None)
--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,34 +1,39 @@
 #!/usr/bin/env python3
+"""Most Recently Used caching module.
 """
-MRUCache module
-"""
+from collections import OrderedDict
 
 from base_caching import BaseCaching
-from collections import OrderedDict
 
 
 class MRUCache(BaseCaching):
-    """ MRUCache is a caching system with MRU eviction policy """
-
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with an MRU
+    removal mechanism when the limit is reached.
+    """
     def __init__(self):
-        """ Initialize MRYCache """
+        """Initializes the cache.
+        """
         super().__init__()
         self.cache_data = OrderedDict()
-        
+
     def put(self, key, item):
-        """ Add an item in the cache """
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
-        if key in self.cache_data:
-            self.cache_data.move_to_end(key)
-        self.cache_data[key] = item
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-            discarded_key, _ = self.cache_data.popitem(last=True)
-            print("DISCARD: {}".format(discarded_key))
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                mru_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", mru_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """ Get an item by key """
-        if key is None or key not in self.cache_data:
-            return None
-        self.cache_data.move_to_end(key)
-        return self.cache_data[key]
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
