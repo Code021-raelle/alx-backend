3e64297fa439d5ab0c62cb68f87e4aae51f5fb37
Code021-raelle <gabrielakinshola3@gmail.com>
2024-07-30 16:18:15 +0100

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,6 +5,13 @@ app = Flask(__name__)
 app.config.from_pyfile('mysettings.cfg')
 babel = Babel(app)
 
+
 app.route('/')
+
+
 def index():
-    render_template('index.html')
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run()
