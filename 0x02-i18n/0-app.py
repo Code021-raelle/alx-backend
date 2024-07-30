d61db18bad6be18f582dc0b016a75b14cd4656d3
Code021-raelle <gabrielakinshola3@gmail.com>
2024-07-30 16:34:19 +0100

    basic flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,15 +1,11 @@
 #!/usr/bin/env python3
+""" Basic flask app """
 from flask import Flask, render_template
-from flask_babel import Babel
 
 app = Flask(__name__)
-app.config.from_pyfile('mysettings.cfg')
-babel = Babel(app)
 
 
-app.route('/')
-
-
+@app.route('/')
 def index():
     return render_template('0-index.html')
 
